5df673883e79c62447417064d8ef033de2432652
sebastianbornacelly <57877305+sirShovo@users.noreply.github.com>
2024-11-29 16:51:35 -0500

    refactor(input): add type and css module

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,30 +1,25 @@
 import React from "react";
-
-interface InputProps {
-  id: string;
-  type: string;
-  placeholder?: string;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  className?: string;
-}
+import styles from "./Input.module.css";
+import { InputProps } from "./Input.types";
 
 export const Input: React.FC<InputProps> = ({
-  id,
   type,
-  placeholder,
+  placeholder = "",
   value,
   onChange,
   className,
+  disabled = false,
 }) => {
   return (
     <input
-      id={id}
       type={type}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      className={`w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 ${className}`}
+      disabled={disabled}
+      className={`${styles.input} ${disabled ? styles.disabled : ""} ${
+        className || ""
+      }`}
     />
   );
 };
---
+++ src/components/atoms/Input/Input.types.ts
@@ -0,0 +1,8 @@
+export type InputProps = {
+  type: "text" | "email" | "password" | "number"; //
+  placeholder?: string;
+  value?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  disabled?: boolean;
+};
