0facde52467d3aeaf65361a1f507793b015d77f0
sebastianbornacelly <57877305+sirShovo@users.noreply.github.com>
2024-11-29 15:47:25 -0500

    chore(app): separate app base

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
-export const App = () => {
-  return <h1 className="text-3xl font-bold underline">Esto es una prueba </h1>;
-};
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Home } from "./pages/Home";
+
+const queryClient = new QueryClient();
+
+export const App: React.FC = () => (
+  <QueryClientProvider client={queryClient}>
+    <Home />
+  </QueryClientProvider>
+);
